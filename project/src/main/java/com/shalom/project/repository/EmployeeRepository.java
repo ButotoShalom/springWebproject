333d16574e2b465e426a052c19d7c0d017327d32
Butoto Shalom <butotoshalom10@gmail.com>
2022-02-06 20:29:26 +0100

    web app
    spring web application

---
+++ project/src/main/java/com/shalom/project/repository/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.shalom.project.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.shalom.project.entity.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+	
+}
